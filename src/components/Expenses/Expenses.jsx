440edc65cf24adcd406e32b5b91ad69128a9184c
Chandler Bowman <doctorgrant99@gmail.com>
2023-06-08 20:39:09 -0600

    added if statement

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -11,7 +11,20 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+  let expensesContent = <p>No expenses found.</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+            key={`${expense.id}`}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+    ))
+  }
   return (
     <div>
       <Card className='expenses'>
@@ -19,14 +32,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense, index) => (
-          <ExpenseItem
-            key={`${expense.id}-${index}`}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );

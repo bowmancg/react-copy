ab3993dc1b6f57a821902d4af61f47b057540561
Chandler Bowman <doctorgrant99@gmail.com>
2023-06-16 16:34:25 -0600

    added button module

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,24 +1,13 @@
-import styled from 'styled-components'
+// import styled from 'styled-components'
+import React from "react";
+import styles from "./Button.module.css"
 
-const Button = styled.button`
-font: inherit;
-padding: 0.5rem 1.5rem;
-border: 1px solid red;
-color: black;
-background: green;
-box-shadow: 0 0 4px black
-cursor: pointer;
-
-&:focus {
-    outline: none;
-}
-
-&:hover,
-&:active {
-    background: blue;
-    border-color: black;
-    box-shadow: 0 0 8px black
-}
-`
-
-export default Button;
+const Button = (props) => {
+    return (
+      <button type={props.type} className={styles.button} onClick={props.onClick}>
+        {props.children}
+      </button>
+    );
+  };
+  
+  export default Button;
